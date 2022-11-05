08468dfd314bc9e6f25cb0f5bb2ef6994ef981a7
Aditya Singh <aditya@cleverstudio.in>
2022-11-05 12:10:40 +0530

    Updated JS and Html files

--- script.js
+++ script.js
@@ -9,13 +9,38 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-async function foo() {
+function checkUrl() {
   var url = document.getElementById("videoSearchBar").value;
+  if (url != undefined || url != "") {
+    var regExp =
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match && match[2].length == 11) {
+      console.log(match[2]);
+      yt(url);
+    } else {
+      console.log("Triggere");
+      if (
+        url.includes("https://fb.watch/") ||
+        url.includes("https://www.facebook.com/reel/")
+      ) {
+        fb(url);
+      } else {
+        console.log("Invalid URL provided");
+      }
+    }
+  }
+}
+
+async function yt(Url) {
+  var url = Url;
   const options = {
     method: "GET",
     headers: {
+      "access-control-allow-origin": "*",
       "X-RapidAPI-Key": "435ec0b7bfmshbf41945eb7fd82cp173a04jsnd5c26f1cb59a",
-      "X-RapidAPI-Host": "youtube-video-download-info.p.rapidapi.com",
+      "X-RapidAPI-Host":
+        "fastest-social-video-and-image-downloader.p.rapidapi.com",
     },
   };
 
@@ -26,15 +51,11 @@ async function foo() {
   }
 
   let res = await fetch(
-    `https://youtube-video-download-info.p.rapidapi.com/dl?id=${url.slice(
-      -11
-    )}`,
+    `https://fastest-social-video-and-image-downloader.p.rapidapi.com/youtube?url=${url}`,
     options
   );
-  console.log(url);
   let Data = await res.json();
-
-  if (Data.status === "fail") {
+  if (Data.success === "false") {
     var loaderid = document.getElementById("preloader");
     loaderid.style.display = "none";
     var errorid = document.getElementById("ErrorVideo");
@@ -49,6 +70,7 @@ async function foo() {
     // element.style.display = "none";
   } else {
     console.log(Data);
+    window.open(Data.thumbnail);
     document.getElementById("down").href = "#Downloadsection";
     var clickbtn = document.getElementById("down");
     clickbtn.click();
@@ -56,46 +78,46 @@ async function foo() {
     loaderid.style.display = "none";
     var loaderid = document.getElementById("ErrorVideo");
     loaderid.style.display = "none";
-    document.querySelector(".title").innerHTML = Data.title;
-    document.querySelector(".responseThumbnail").src = Data.thumb;
-    document.querySelector(".author1").innerHTML = "By " + Data.author.bold();
-    document.querySelector(".quality1").innerHTML =
-      "Download " + Data.link[17][3];
-    document.querySelector(".size1").innerHTML =
-      Data.link[17][1] != "" ? Data.link[17][1] : "Auto";
-    document.querySelector(".qualityBtnbox1").href = Data.link[17][0];
-    document.querySelector(".quality2").innerHTML =
-      "Download " + Data.link[18][3];
-    document.querySelector(".size2").innerHTML =
-      Data.link[18][1] != "" ? Data.link[18][1] : "Auto";
-    document.querySelector(".qualityBtnbox2").href = Data.link[18][0];
-    document.querySelector(".quality3").innerHTML =
-      "Download " + Data.link[22][3];
-    document.querySelector(".size3").innerHTML =
-      Data.link[22][1] == "" ? "Auto" : Data.link[22][1];
-    document.querySelector(".qualityBtnbox3").href = Data.link[22][0];
+    document.querySelector(".title").innerHTML =
+      Data.title == "" ? "Untitled Video" : Data.title;
+    document.querySelector(".responseThumbnail").src = Data.thumbnail;
+    document.querySelector(".author1").innerHTML = "By Youtube";
+    document.querySelector(".quality1").innerHTML = "Download 360P";
+    document.querySelector(".size1").innerHTML = "Auto";
+    document.querySelector(".qualityBtnbox1").href = Data.data["360p"];
+    document.querySelector(".quality2").innerHTML = "Download 720P";
+    document.querySelector(".size2").innerHTML = "Auto";
+    document.querySelector(".qualityBtnbox2").href = Data.data["720p"];
+    // document.querySelector(".quality3").innerHTML =
+    //   "Download " + Data.link[22][3];
+    // document.querySelector(".size3").innerHTML =
+    //   Data.link[22][1] == "" ? "Auto" : Data.link[22][1];
+    // document.querySelector(".qualityBtnbox3").href = Data.link[22][0];
     var element = document.querySelector(".DownloadSection");
     element.style.display = "flex";
+    var hidebtn = document.getElementById("btn3");
+    hidebtn.style.display = "none";
   }
 }
 
-async function insta() {
-  var url = document.getElementById("videoSearchBar").value;
+async function fb(Url) {
+  var url = Url;
   const options = {
     method: "GET",
-    url: "https://instagram-looter2.p.rapidapi.com/post",
     headers: {
       "X-RapidAPI-Key": "435ec0b7bfmshbf41945eb7fd82cp173a04jsnd5c26f1cb59a",
-      "X-RapidAPI-Host": "instagram-looter2.p.rapidapi.com",
+      "X-RapidAPI-Host": "facebook-reel-and-video-downloader.p.rapidapi.com",
     },
   };
 
   let res = await fetch(
-    `https://instagram-looter2.p.rapidapi.com/post?link=${url}`,
+    `https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php?url=${url}`,
     options
   );
   let Data = await res.json();
-
+  if (Data.success == "Fail") {
+    console.log("Failed");
+  }
   if (Data.status === "fail") {
     var loaderid = document.getElementById("preloader");
     loaderid.style.display = "none";
@@ -106,39 +128,41 @@ async function insta() {
     document.getElementById("down").href = "#ErrorVideo";
     var clickbtn = document.getElementById("down");
     clickbtn.click();
-    // document.getElementsByClassName("downloadSection");
-    // var element = document.querySelector(".downloadSection");
-    // element.style.display = "none";
   } else {
-    console.log(Data);
-    console.log(Data.video_url);
-    console.log(Data.thumbnail_src);
-    document.getElementById("down").href = "#Downloadsection";
-    var clickbtn = document.getElementById("down");
-    clickbtn.click();
-    var loaderid = document.getElementById("preloader");
-    loaderid.style.display = "none";
-    var loaderid = document.getElementById("ErrorVideo");
-    loaderid.style.display = "none";
-    document.querySelector(".title").innerHTML = Data.title;
-    document.querySelector(".responseThumbnail").src = Data.thumbnail_src;
-    document.querySelector(".author1").innerHTML = "By " + Data.author.bold();
-    document.querySelector(".quality1").innerHTML =
-      "Download " + Data.link[17][3];
-    document.querySelector(".size1").innerHTML =
-      Data.link[17][1] != "" ? Data.link[17][1] : "Auto";
-    document.querySelector(".qualityBtnbox1").href = Data.link[17][0];
-    document.querySelector(".quality2").innerHTML =
-      "Download " + Data.link[18][3];
-    document.querySelector(".size2").innerHTML =
-      Data.link[18][1] != "" ? Data.link[18][1] : "Auto";
-    document.querySelector(".qualityBtnbox2").href = Data.link[18][0];
-    document.querySelector(".quality3").innerHTML =
-      "Download " + Data.link[22][3];
-    document.querySelector(".size3").innerHTML =
-      Data.link[22][1] == "" ? "Auto" : Data.link[22][1];
-    document.querySelector(".qualityBtnbox3").href = Data.link[22][0];
-    var element = document.querySelector(".DownloadSection");
-    element.style.display = "flex";
+    if (Data.success == false) {
+      var loaderid = document.getElementById("preloader");
+      loaderid.style.display = "none";
+      var errorid = document.getElementById("ErrorVideo");
+      errorid.style.display = "flex";
+      var element = document.querySelector(".DownloadSection");
+      element.style.display = "none";
+      document.getElementById("down").href = "#ErrorVideo";
+      var clickbtn = document.getElementById("down");
+      clickbtn.click();
+    } else {
+      document.getElementById("down").href = "#Downloadsection";
+      var clickbtn = document.getElementById("down");
+      clickbtn.click();
+      var loaderid = document.getElementById("preloader");
+      loaderid.style.display = "none";
+      var loaderid = document.getElementById("ErrorVideo");
+      loaderid.style.display = "none";
+      var element = document.querySelector(".DownloadSection");
+      element.style.display = "flex";
+      document.querySelector(".title").innerHTML =
+        Data.title == "" ? "Untitled Video" : Data.title;
+      document.querySelector(".responseThumbnail").src = Data.thumbnail;
+      document.querySelector(".author1").innerHTML = "By Facebook User";
+      document.querySelector(".quality1").innerHTML = "Download High Quality";
+      document.querySelector(".size1").innerHTML = "Auto";
+      document.querySelector(".qualityBtnbox1").href =
+        Data.links["Download High Quality"];
+      document.querySelector(".quality2").innerHTML = "Download Low Quality";
+      document.querySelector(".size2").innerHTML = "Auto";
+      document.querySelector(".qualityBtnbox2").href =
+        Data.links["Download Low Quality"];
+      var hidebtn = document.getElementById("btn3");
+      hidebtn.style.display = "none";
+    }
   }
 }
